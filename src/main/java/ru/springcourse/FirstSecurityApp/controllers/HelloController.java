7e8b8517d23fb64d96db2815e94ecb7689148715
Denis Rogov <kpotuw@gmail.com>
2023-05-20 12:50:34 +0300

    Lesson 79. Init. Authentication with Spring Security.

---
+++ src/main/java/ru/springcourse/FirstSecurityApp/controllers/HelloController.java
@@ -0,0 +1,24 @@
+package ru.springcourse.FirstSecurityApp.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.springcourse.FirstSecurityApp.security.PersonDetails;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    private String sayHello() {
+        return  "hello";
+    }
+
+    @GetMapping("/showUserInfo")
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+
+        return "hello";
+    }
+}
